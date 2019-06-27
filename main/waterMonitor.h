193daf6e5a2358e2eaa715e6b7708986a34502e9
brianinnes <binnes@uk.ibm.com>
2019-06-27 19:20:50 +0100

    updated defs in header

--- main/waterMonitor.h
+++ main/waterMonitor.h
@@ -1,9 +1,14 @@
+#include "freertos/event_groups.h"
 #include <mqtt_client.h>
 
 extern const char *MQTT_TAG;
 extern const char TAG[];
 extern esp_mqtt_client_handle_t client;
 extern SemaphoreHandle_t xMQTTClientMutex;
+extern const int CONNECTED_BIT;
+extern EventGroupHandle_t wifi_event_group;
 
 extern void flow_init();
 extern void wifiStart();
+extern void stop_WebServer();
+extern void start_WebServer();
